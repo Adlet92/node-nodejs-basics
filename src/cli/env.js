68f84c79b80e6b0769a02e209d367ca166969dc7
adletb7 <adlet.baiteliyev@gmail.com>
2024-01-29 19:00:50 +0600

    env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVariables = process.env;
+
+  const rssEnvVariables = Object.entries(envVariables)
+    .filter(([key]) => key.startsWith('RSS_'));
+
+  const formattedVariables = rssEnvVariables.map(([key, value]) => `${key}=${value}`).join('; ');
+  console.log('Parsed RSS environment variables:', formattedVariables);
 };
 
 parseEnv();
