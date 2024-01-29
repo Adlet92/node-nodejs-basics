d99bf96f878fdd6c96931d6e90afaba5cde0a1e1
adletb7 <adlet.baiteliyev@gmail.com>
2024-01-29 18:53:18 +0600

    list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  const folderPath = join(__dirname, 'files');
+
+  try {
+    await fs.promises.access(folderPath, fs.constants.R_OK);
+
+    const filenames = await fs.promises.readdir(folderPath);
+
+    console.log('List of filenames in "files" folder:', filenames);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error('FS operation failed: Folder does not exist');
+    }
+    console.error('FS operation failed:', error.message);
+  }
 };
 
 await list();
