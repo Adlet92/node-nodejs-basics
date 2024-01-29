1a2b5e03ed40e29717e88b0433a7c494a7b4ff6b
adletb7 <adlet.baiteliyev@gmail.com>
2024-01-29 18:50:29 +0600

    files

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fileToRemove.txt');
+
+  try {
+    await fs.promises.access(filePath, fs.constants.R_OK | fs.constants.W_OK);
+
+    await fs.promises.unlink(filePath);
+    console.log('File deleted successfully: fileToRemove.txt');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error('FS operation failed: File does not exist');
+    }
+
+    console.error('FS operation failed:', error.message);
+  }
 };
 
 await remove();
